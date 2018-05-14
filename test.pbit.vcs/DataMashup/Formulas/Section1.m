1e4703845109b72458df26811db48d62e0b8ef53
dams822 <dams822@gmail.com>
2018-05-14 22:41:12 +0200

    the commit before the 1st merge

--- test.pbit.vcs/DataMashup/Formulas/Section1.m
+++ test.pbit.vcs/DataMashup/Formulas/Section1.m
@@ -5,3 +5,9 @@ shared Table1 = let
     #"Changed Type" = Table.TransformColumnTypes(Source,{{"Column1", Int64.Type}})
 in
     #"Changed Type";
+
+shared Table2 = let
+    Source = Table.FromRows(Json.Document(Binary.Decompress(Binary.FromText("i45WMlSK1YlWMgKTJsZgyhQbB10wFgA=", BinaryEncoding.Base64), Compression.Deflate)), let _t = ((type text) meta [Serialized.Text = true]) in type table [Column1 = _t]),
+    #"Changed Type" = Table.TransformColumnTypes(Source,{{"Column1", Int64.Type}})
+in
+    #"Changed Type";
